08fa2bdf8b84e95a88be332465e6ad22e7dc5b87
shota <shota@Ohno-Shotaro>
2025-05-02 01:32:26 +0900

    課題　⑥週目

--- src/product/Main.java
+++ src/product/Main.java
@@ -1,17 +1,18 @@
 package product;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
 		ProductManager manager = new ProductManager();
 
-		manager.addProduct(new Product(1, "冷蔵庫", 50000, 10));
-		manager.addProduct(new DiscountedProduct(2, "ソファ", 30000, 5, 0.3));
-		manager.addProduct(new Product(3, "米", 2000, 3));
-		manager.addProduct(new Product(4, "小説", 1500, 4));
-		manager.addProduct(new Product(5, "Tシャツ", 1500, 5));
+		//manager.addProduct(new Product(1, "冷蔵庫", 50000, 10));
+		//manager.addProduct(new DiscountedProduct(2, "ソファ", 30000, 5, 0.3));
+		//manager.addProduct(new Product(3, "米", 2000, 3));
+		//manager.addProduct(new Product(4, "小説", 1500, 4));
+		//manager.addProduct(new Product(5, "Tシャツ", 1500, 5));
 
 		System.out.println("---商品を5つ追加して全てを表示する---");
 		for (Product product : manager.getAllProducts()) {
@@ -48,5 +49,60 @@ public class Main {
 		} else {
 			System.out.println("商品「Tシャツ」は見つかりませんでした。");
 		}
+		
+		
+//⑥週目課題
+        Scanner scanner = new Scanner(System.in);
+		
+		boolean running = true;
+		
+		while (running) {
+			try {
+				System.out.println("--メニュ---");
+				System.out.println("1:商品追加");
+				System.out.println("2:商品情報取得");
+				System.out.println("3:商品検索");
+				System.out.println("4:商品全て表示");
+				System.out.println("5:商品削除");
+				System.out.println("0:終了");
+				
+				System.out.println("\nメニューから操作を選択してください。");
+				
+				int choice = Integer.parseInt(scanner.nextLine());
+				
+				switch (choice) {
+				case 1://商品追加
+					manager.registerProduct(scanner);
+					break;
+					
+				case 2://商品情報取得
+					manager.getProductInfo(scanner);
+					break;
+					
+				case 3://商品検索
+					manager.searchProduct(scanner);
+					break;
+					
+				case 4://商品全て表示
+					manager.showProducts(scanner);
+					break;
+					
+				case 5://商品削除
+					manager.deleteProduct(scanner);
+					break;
+					
+				case 0://終了
+					running = false;
+					System.out.println("終了します");
+					break;
+				default:
+					System.out.println("\nメニューから操作を選択してください。");
+				}
+				
+			} catch(NumberFormatException e) {
+				System.out.println("無効な入力です。操作は数字で選択してください。");
+			}
+		}
+		scanner.close();
 	}
 }
--- src/product/ProductManager.java
+++ src/product/ProductManager.java
@@ -1,10 +1,11 @@
 package product;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class ProductManager implements Searchable {
-
 
 	private List<Product> products;
 
@@ -37,7 +38,7 @@ public class ProductManager implements Searchable {
 		}
 		return null;
 	}
-	
+
 	public List<Product> getAllProducts() {
 		return products;
 
@@ -46,14 +47,152 @@ public class ProductManager implements Searchable {
 	//*⑤週目課題
 	// searchableインターフェース
 	@Override
-		public List<Product> search(String keyword) {
+	public List<Product> search(String keyword) {
 		List<Product> result = new ArrayList<>();
-		
-			for (Product product : products) {
-				if (product.getName().contains(keyword)) {
-					result.add(product);
-				}
+
+		for (Product product : products) {
+			if (product.getName().contains(keyword)) {
+				result.add(product);
 			}
-			return result.isEmpty() ? null : result;
+		}
+		return result.isEmpty() ? null : result;
+	}
+
+	//⑥週目課題
+	//1:商品追加メソッド
+	public void registerProduct(Scanner scanner) {
+		try {
+
+			System.out.println("商品IDを入力してください: ");
+			int id = Integer.parseInt(scanner.nextLine());
+			System.out.println("入力された商品ID:" + id);
+
+			System.out.println("商品名を入力してください: ");
+			String name = scanner.nextLine();
+			validateProductName(name);
+			System.out.println("入力された商品名:" + name);
+
+			System.out.println("価格を入力してください: ");
+			int price = validatePrice(scanner.nextLine());
+			System.out.println("入力された価格:" + price);
+
+			System.out.println("在庫数を入力してください: ");
+			int stock = validateStock(scanner.nextLine());
+			System.out.println("入力された在庫:" + stock);
+
+			products.add(new Product(id, name, price, stock));
+			System.out.println(
+					"Product: id=" + id + ", name=" + name + ", price=" + price + ", stock=" + stock + "を登録しました。");
+		} catch (NumberFormatException e) {
+			System.out.println("入力エラー: " + e.getMessage());
 		}
 	}
+
+	// 商品名チェック
+	private void validateProductName(String name) {
+		if (name == null || name.trim().isEmpty()) {
+			throw new IllegalArgumentException("無効な入力です。商品名を正しく入力してください。");
+		}
+	}
+
+	// 価格チェック
+	private int validatePrice(String input) {
+		try {
+			int price = Integer.parseInt(input);
+			if (price < 0) {
+				throw new IllegalArgumentException("無効な入力です。価格を正しく入力してください。");
+			}
+			return price;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("無効な入力です。価格は数字で入力してください。");
+		}
+	}
+
+	// 在庫数チェック
+	private int validateStock(String input) {
+		try {
+			int stock = Integer.parseInt(input);
+			if (stock < 0) {
+				throw new IllegalArgumentException("無効な入力です。在庫を正しく入力してください。");
+			}
+			return stock;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("無効な入力です。在庫数は数字で入力してください。");
+		}
+	}
+
+	//2:商品情報取得メソッド
+	public void getProductInfo(Scanner scanner) {
+		System.out.println("商品情報を取得する商品名を入力してください: ");
+		String keyword = scanner.nextLine();
+		boolean found = false;
+
+		for (Product product : products) {
+			if (product.getName().equalsIgnoreCase(keyword)) {
+				System.out.println("取得した商品は、product: id=" + product.getId() + ", name=" + product.getName() + ", price="
+						+ product.getPrice() + ", stock=" + product.getStock() + "です。");
+				found = true;
+			}
+		}
+
+		if (!found) {
+			System.out.println("商品が見つかりませんでした。");
+		}
+	}
+
+	//3:商品検索メソッド
+	public void searchProduct(Scanner scanner) {
+		System.out.println("検索する商品名を入力してください: ");
+		String keyword = scanner.nextLine();
+		boolean found = false;
+
+		for (Product product : products) {
+			if (product.getName().contains(keyword)) {
+				System.out.println(product);
+				found = true;
+			}
+		}
+
+		if (!found) {
+			System.out.println("商品が見つかりませんでした。");
+		}
+	}
+
+	//4:商品全て表示メソッド
+	public void showProducts(Scanner scanner) {
+		if (products.isEmpty()) {
+			System.out.println("まだ商品は登録されていません。");
+		} else {
+			System.out.println("商品を全て表示します");
+			for (Product product : products) {
+				System.out.println(product);
+			}
+		}
+	}
+
+	//5:商品削除メソッド
+	public void deleteProduct(Scanner scanner) {
+		System.out.println("削除する商品のIDを入力してください: ");
+		try {
+			int targetId = Integer.parseInt(scanner.nextLine());
+			Iterator<Product> iterator = products.iterator();
+			boolean deleted = false;
+
+			while (iterator.hasNext()) {
+				Product product = iterator.next();
+				if (product.getId() == targetId) {
+					iterator.remove();
+					System.out.println("商品IDが" + targetId + "の商品を削除しました");
+					deleted = true;
+					break;
+				}
+			}
+
+			if (!deleted) {
+				System.out.println("指定された商品は見つかりませんでした。");
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("商品が見つかりませんでした。");
+		}
+	}
+}
